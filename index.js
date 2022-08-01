9cbf109573530b8adb659241248c1f95690dbfbf
Anthony <cataldoanthony2@gmail.com>
2022-07-31 21:45:32 -0400

    commit tree a4590985ec02956fff36cc9ce9cec19a9c2fc22b

---
+++ index.js
@@ -0,0 +1,18 @@
+function sayHello() {
+    console.log("Hello!");
+  } sayHello()
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  } sayHelloToGuadalupe();
+  sayHelloToLiz();
+  sayHelloToSamip();
+  function doSomething(thing) {
+    console.log(thing);
+  }
+  doSomething("Welcome");
